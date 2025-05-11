363b902394a53b10c6c6bd8fa13b5b8edf0777a8
httdjuly <thuyduong3042@gmail.com>
2025-05-11 15:54:09 +0700

    init

---
+++ DiaryApp/app/src/main/java/com/example/diaryloveproject/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.diaryloveproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+
+            // Áp dụng thêm padding XML của bạn + padding hệ thống
+            v.setPadding(
+                    systemBars.left + 32,   // hoặc thay bằng: v.getPaddingLeft() + systemBars.left
+                    systemBars.top + 48,
+                    systemBars.right + 32,
+                    systemBars.bottom + 48
+            );
+
+            return insets;
+        });
+    }
+}
